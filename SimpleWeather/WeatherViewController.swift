7713290065f38d1c26d285d0a443d9ee2520e698
Sebastian C <crossasebastian@gmail.com>
2017-06-11 18:21:58 -0600

    Added the VC for the seconds screen. Finished up the graphics on both of them

---
+++ SimpleWeather/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherViewController.swift
+//  SimpleWeather
+//
+//  Created by Sebastian Crossa on 6/11/17.
+//  Copyright © 2017 CROSS-A. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Weather View Controller")
+        
+    }
+
+//    override var prefersStatusBarHidden: Bool {
+//        return false
+//    }
+
+}
