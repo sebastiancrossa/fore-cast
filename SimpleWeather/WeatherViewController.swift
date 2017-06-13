67f493255008fedc0f74d344517c64720a06a188
Sebastian C <crossasebastian@gmail.com>
2017-06-12 21:33:51 -0600

    Fixed bug where user could search without writing on the UITextField

--- SimpleWeather/WeatherViewController.swift
+++ SimpleWeather/WeatherViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+/*
+ TODO: Fix bug where users can leave the space blank and it will search for the weather of London
+*/
+
 class WeatherViewController: UIViewController {
 
     @IBOutlet var userCityInput: UITextField!
@@ -25,63 +29,71 @@ class WeatherViewController: UIViewController {
     
     @IBAction func weatherSearch(_ sender: Any) {
         
-        forecastTitle.text = "Today's forecast is:"
-        view.endEditing(true)
-        
-        // URL that connects to the API so we can retrieve data
-        // If user does not update the text field we will return an error
-        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + userCityInput.text!.replacingOccurrences(of: " ", with: "%20") + ",uk&appid=5a14e6c17441b978de09f593ee02c8bd") {
-        
-            // Creating a task for the URL so we can get data from it
-            let task = URLSession.shared.dataTask(with: url) {
-                data, response, error in
+        if userCityInput.hasText == true  {
+            
+            forecastTitle.text = "Today's forecast is:"
+            view.endEditing(true)
+            
+            // URL that connects to the API so we can retrieve data
+            // If user does not update the text field we will return an error
+            if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + userCityInput.text!.replacingOccurrences(of: " ", with: "%20") + ",uk&appid=5a14e6c17441b978de09f593ee02c8bd") {
                 
-                // See if we encounter any error while trying to retrieve the information
-                if error == nil {
+                // Creating a task for the URL so we can get data from it
+                let task = URLSession.shared.dataTask(with: url) {
+                    data, response, error in
                     
-                    // Variable that has the direct access to the url we are trying to connect to
-                    if let contentURL = data {
+                    // See if we encounter any error while trying to retrieve the information
+                    if error == nil {
                         
-                        // Processing the JSON retrieved from the weather API
-                        do {
+                        // Variable that has the direct access to the url we are trying to connect to
+                        if let contentURL = data {
                             
-                            // Variable that will handles the direct result of the JSON
-                            let jsonResult = try JSONSerialization.jsonObject(with: contentURL, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String: AnyObject]
-                            
-                            print(jsonResult)
-                            
-                            if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                            // Processing the JSON retrieved from the weather API
+                            do {
                                 
-                                // We dispatch so we don't have to constantly wait
-                                DispatchQueue.main.sync(execute: {
+                                // Variable that will handles the direct result of the JSON
+                                let jsonResult = try JSONSerialization.jsonObject(with: contentURL, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String: AnyObject]
+                                
+                                print(jsonResult)
+                                
+                                if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
                                     
-                                    self.weatherDescription.text = description
+                                    // We dispatch so we don't have to constantly wait
+                                    DispatchQueue.main.sync(execute: {
+                                        
+                                        self.weatherDescription.text = description
+                                        
+                                    })
                                     
-                                })
+                                }
+                                
+                            } catch {
+                                
+                                // Error handling if JSON processing fails
+                                print("-- Weather Conditionshas encountered an error while tryiong to proccess the JSON file. (TIP: Double check if you are directing to a JSON file)")
                                 
                             }
                             
-                        } catch {
-                            
-                            // Error handling if JSON processing fails
-                            print("-- Weather Conditionshas encountered an error while tryiong to proccess the JSON file. (TIP: Double check if you are directing to a JSON file)")
-                            
                         }
                         
+                    } else {
+                        
+                        print("-- Weather Conditions has encountered an error : \(error)")
+                        
                     }
-                    
-                } else {
-                    
-                    print("-- Weather Conditions has encountered an error : \(error)")
-                    
                 }
+                
+                task.resume()
+                
+            } else {
+                
+                forecastTitle.text = "City needed for search"
+                
             }
             
-            task.resume()
-        
         } else {
             
-            forecastTitle.text = "City required for search"
+            forecastTitle.text = "City needed for search"
             
         }
         
